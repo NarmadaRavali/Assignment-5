9e54fc92842112d088a5d767a652b67942064b24
Ravikanth Dodda <rdodda1@asu.edu>
2021-03-09 21:30:52 -0700

    changed constants

--- src/controller/CommonConstants.java
+++ src/controller/CommonConstants.java
@@ -10,11 +10,12 @@ import java.awt.*;
  */
 public class CommonConstants {
 
-    public final int FRAME_WIDTH = 1000, FRAME_HEIGHT = 800;
-    public final int SYMBOL_WIDTH = 180;
-    public final int SYMBOL_HEIGHT = 80;
-    public final String LEFT_PANEL_NAME = "Left Panel",
+    public static final int FRAME_WIDTH = 1000, FRAME_HEIGHT = 800;
+    public static final int SYMBOL_WIDTH = 180;
+    public static final int SYMBOL_HEIGHT = 80;
+    public static final String LEFT_PANEL_NAME = "Left Panel",
             RIGHT_PANEL_NAME = "Right Panel";
-    public final String SAVE = "Save", LOAD = "Load", SPACE = "New Space";
-    public Color LIGHT_GREY = new Color(235, 235, 235);
+    public static final String SAVE = "Save", LOAD = "Load", SPACE = "New " +
+            "Space";
+    public static final Color LIGHT_GREY = new Color(235, 235, 235);
 }
